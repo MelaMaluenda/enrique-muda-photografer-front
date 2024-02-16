9896b53980527b240dab64309ba8ae1f6961ee65
Mela Maluenda <maluenda.mela@gmail.com>
2024-02-16 19:46:33 +0100

    Add style types

---
+++ src/Styles/mainTheme.ts
@@ -0,0 +1,18 @@
+import { DefaultTheme } from "styled-components";
+
+const mainTheme: DefaultTheme = {
+  colors: {
+    mainText: "#213327",
+  },
+  typography: {
+    mainFontFamily: "'Playfair Display', serif",
+    regularFontFamily: "'Source Sans 3', sans-serif",
+    superSize: "2rem",
+    mainSize: "1.5rem",
+    titleSize: "1.25rem",
+    secondaryTitleSize: "1.125rem",
+    regularSize: "1rem",
+  },
+};
+
+export default mainTheme;
